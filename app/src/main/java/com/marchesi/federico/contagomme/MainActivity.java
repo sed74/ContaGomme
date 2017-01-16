8a153fc86fd9e8dec7e6301ed35aa44b9e403f9a
Federico <fede.sed74@gmail.com>
2017-01-16 22:36:35 +0100

    Initial

---
+++ app/src/main/java/com/marchesi/federico/contagomme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.marchesi.federico.contagomme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
